7883b7dc52999792c194c2e4efe102114be9c247
k4-100 <k4-100@protonmail.com>
2022-08-18 15:39:43 +0200

    Basic globals

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("run");
+// Globals - no window!!!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+console.log("__dirname: ", __dirname);
+console.log("__filename: ", __filename);
+console.log("require: ", require);
+console.log("\n########################## \n");
+console.log("module: ", module);
+console.log("\n########################## \n");
+// node, v8 version, system info
+// console.log("process: ", process);
