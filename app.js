15ab02a64d025f63699ce862e9c927ecd4d4fbe8
k4-100 <k4-100@protonmail.com>
2022-08-23 14:21:46 +0200

    Added parameters to EventEmitter

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const EventEmitter = require("events");
 
 const customEmitter = new EventEmitter();
 
-customEmitter.on("response", () => {
-  console.log("called response");
+customEmitter.on("response", (message) => {
+  console.log(`called response, ${message}`);
 });
 
 const flag = process.argv[2];
 
-if (flag === "hello") customEmitter.emit("response");
+if (flag === "hello") customEmitter.emit("response", (message = "text"));
